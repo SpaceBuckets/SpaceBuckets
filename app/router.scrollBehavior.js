37adadfedcfa0f4cd67cdfc223c4ac5e2fa1b5d8
ekrof <info@spacebuckets.com>
2020-10-17 17:47:17 -0300

    a

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,3 +1,9 @@
 export default function (to, from, savedPosition) {
+  if (to.hash) {
+    return {selector: to.hash}
+  } else if (savedPosition) {
+    return savedPosition
+  } else {
     return { x: 0, y: 0 }
   }
+}
