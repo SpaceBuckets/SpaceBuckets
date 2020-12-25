770b07d91f1676fdc56a8d3bb3a07739888c7d23
ekrof <info@spacebuckets.com>
2020-12-25 14:47:15 -0300

    size limit

--- pages/index.vue
+++ pages/index.vue
@@ -64,7 +64,7 @@ export default {
   async asyncData({ params }) {
     const mad = await singlePost("the-mad-scientist");
     const forever = await singlePost("budsforever");
-    const ekrof = await singlePost("next-gen");
+    const ekrof = await singlePost("potcasso");
     const simpleLED = await singlePost("bruteLED");
     const gnk = await singlePost("GNKtote");
     const sag = await singlePost("SuperAngryGuy");
