c8f0aac0195591bc1a245dc28a38c3391de64acc
SpaceBuckets <info@spacebuckets.com>
2021-04-18 17:10:41 -0300

    b

--- components/postmasonry.vue
+++ components/postmasonry.vue
@@ -107,7 +107,7 @@
       v-for="(img, ie) in post.i"
       :key="`section-${ie}`"
     >
-      <img v-if="variation != 'skeleton'" :src="`/builds/${post.s}/${img.h}`" alt />
+      <img :src="`/builds/${post.s}/${img.h}`" alt />
       <img v-if="variation === 'skeleton'" loading="lazy" :src="`/builds/${post.s}/${img.h}`" alt />
       <div :style="{ 'padding-top': img.s + '%', 'background': '#'+img.h.replace('.jpg', '') }"></div>
     </div>
