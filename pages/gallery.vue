fcd7324c20392b4925e00659bae9451b0e6c1997
SpaceBuckets <info@spacebuckets.com>
2021-04-18 20:46:21 -0300

    b

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -344,7 +344,7 @@ export default {
   head() {
     return {
       title: `Space Buckets - GALLERY`,
-      link: [ { rel: "canonical", href: "https://spacebuckets.com" + this.$route.path, },],
+      link: [ { rel: "canonical", href: "https://spacebuckets.com" + this.$route.path + "/", },],
       meta: [{ hid: 'description', name: 'description', content: 'Browse the collection of DIY indoor gardens from the community. More than 350 builds await!' }],
 
     };
