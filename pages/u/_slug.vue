09a087066e940567552aa9b7bbe75eb943837e92
SpaceBuckets <info@spacebuckets.com>
2021-04-18 18:02:24 -0300

    b

--- pages/u/_slug.vue
+++ pages/u/_slug.vue
@@ -17,6 +17,7 @@ export default {
   scrollToTop: false,
   async asyncData({ params }) {
     const post = await singlePost(params.slug);
+    console.log(post)
     if (post.z !== "" || (post.z !== undefined && post.z.length > 0)) {
       post.itemCount = post.z.split(",").length;
     }
