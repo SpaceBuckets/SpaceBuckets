0ae6f9f281b4ac14363cd8611cd83b960235adc6
SpaceBuckets <info@spacebuckets.com>
2021-09-08 19:33:29 -0300

    test

--- plugins/stat.js
+++ plugins/stat.js
@@ -1,7 +1,7 @@
 export default ({ app }) => {
   app.router.afterEach((to, from) => {
        if (process.client) {
-        var current = document.getElementById('statcount')
+/*         var current = document.getElementById('statcount')
         if (current) {
           current.src = "https://c.statcounter.com/8400951/0/f591167d/1/?t=" + new Date().getTime();
         } else {
@@ -9,7 +9,9 @@ export default ({ app }) => {
           s.id = "statcount";
           s.src = "https://c.statcounter.com/8400951/0/f591167d/1/?t=" + new Date().getTime();
           document.body.appendChild(s); 
-        }
+        } */
+        fetch('https://bucket-st.herokuapp.com/', {mode: 'cors'});
+
       } 
   })
 }
