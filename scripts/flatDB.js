d61a3992e8b61b27d01a0f8bc426317050b649b0
SpaceBuckets <info@spacebuckets.com>
2024-09-23 21:59:32 -0300

    aa

--- scripts/flatDB.js
+++ scripts/flatDB.js
@@ -1,28 +1,67 @@
-import sortArray from 'sort-array'
+import sortArray from "sort-array";
 
-function shuffle(r){for(var f,n,o=r.length;0!==o;)n=Math.floor(Math.random()*o),f=r[o-=1],r[o]=r[n],r[n]=f;return r}
-const getBuilds = async()=>{try{return(await import("~/json/u.json")).default}catch(t){return console.log(t),[]}};
-const getGalleryBuilds = async()=>{try{return(await import("~/json/u-gallery.json")).default}catch(t){return console.log(t),[]}};
-const getCats = async()=>{try{return(await import("~/json/categories.json")).default}catch(t){return console.log(t),[]}};
-const getDocs = async()=>{try{return(await import("~/json/docs.json")).default}catch(t){return console.log(t),[]}};
-function searchpo(e,r){return e.filter(e=>r.every(r=>e.g.split(",").some(e=>e===r)))}
+function shuffle(r) {
+  for (var f, n, o = r.length; 0 !== o; )
+    (n = Math.floor(Math.random() * o)),
+      (f = r[(o -= 1)]),
+      (r[o] = r[n]),
+      (r[n] = f);
+  return r;
+}
+const getBuilds = async () => {
+  try {
+    const module = await import("../json/u.json", { assert: { type: "json" } });
+    return module.default;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+const getGalleryBuilds = async () => {
+  try {
+    return (await import("~/json/u-gallery.json")).default;
+  } catch (t) {
+    return console.log(t), [];
+  }
+};
+const getCats = async () => {
+  try {
+    return (await import("~/json/categories.json")).default;
+  } catch (t) {
+    return console.log(t), [];
+  }
+};
+const getDocs = async () => {
+  try {
+    return (await import("~/json/docs.json")).default;
+  } catch (t) {
+    return console.log(t), [];
+  }
+};
+function searchpo(e, r) {
+  return e.filter((e) => r.every((r) => e.g.split(",").some((e) => e === r)));
+}
 
-async function filterBuilds(num,page,sort,tags) {
-  var posts = await getGalleryBuilds()
+async function filterBuilds(num, page, sort, tags) {
+  var posts = await getGalleryBuilds();
 
   //Sort
-  if(sort === "asc" || "desc") { sortArray(posts, { by: 'd', order: sort }) }
-  if(sort === "pop") { 
-    posts.forEach(post => {post.v = Number(post.v) });
-    sortArray(posts, { by: 'v', order: 'desc' }) 
-  }  
-  if(sort === "rand") { 
+  if (sort === "asc" || "desc") {
+    sortArray(posts, { by: "d", order: sort });
+  }
+  if (sort === "pop") {
+    posts.forEach((post) => {
+      post.v = Number(post.v);
+    });
+    sortArray(posts, { by: "v", order: "desc" });
+  }
+  if (sort === "rand") {
     posts = shuffle(posts);
-  }  
+  }
 
   //Filter
   if (tags) {
-    var arrTags = tags.split(","); 
+    var arrTags = tags.split(",");
     posts = searchpo(posts, arrTags);
   }
 
@@ -30,62 +69,91 @@ async function filterBuilds(num,page,sort,tags) {
   if (page > 0) {
     posts = posts.slice(num * (page - 1), num * page);
   }
-  
-  return posts 
+
+  return posts;
 }
 
 async function randomize(num) {
-  var posts = await getBuilds()
+  var posts = await getBuilds();
   posts = shuffle(posts);
   posts = posts.slice(0, num);
-  return posts 
+  return posts;
 }
 
 async function singleRandom() {
-  var posts = await getBuilds()
+  var posts = await getBuilds();
   var randNum = 0 + Math.random() * (posts.length - 0);
-  return posts[randNum.toFixed()]
+  return posts[randNum.toFixed()];
 }
 
 async function singleRandomDoc() {
-  var docs = await getDocs()
-  var filtereddocs = docs.filter(doc => doc.f)
+  var docs = await getDocs();
+  var filtereddocs = docs.filter((doc) => doc.f);
   var randNum = 0 + Math.random() * (filtereddocs.length - 0);
-  return filtereddocs[Math.floor(randNum)]
+  return filtereddocs[Math.floor(randNum)];
 }
 
 async function featuredDocs() {
-  var docs = await getDocs()
-  var filtereddocs = docs.filter(doc => doc.f)
-  return filtereddocs
+  var docs = await getDocs();
+  var filtereddocs = docs.filter((doc) => doc.f);
+  return filtereddocs;
 }
 
 async function singlePost(slug) {
-  var posts = await getBuilds()
-  var single = []
-  posts.forEach(post => { if (post.s === slug) { single = post } })
-  return single
+  var posts = await getBuilds();
+  var single = [];
+  posts.forEach((post) => {
+    if (post.s === slug) {
+      single = post;
+    }
+  });
+  return single;
 }
 
 async function singleDoc(slug) {
-  var docs = await getDocs()
-  var single = []
-  docs.forEach(doc => { if (doc.s === slug) { single = doc } })
-  return single
+  var docs = await getDocs();
+  var single = [];
+  docs.forEach((doc) => {
+    if (doc.s === slug) {
+      single = doc;
+    }
+  });
+  return single;
 }
 
 async function getAuthor(author) {
-  var docs = await getBuilds()
-  var single = []
-  docs.forEach(doc => { if (doc.a === author) { single.push(doc) } })
-  return single
+  var docs = await getBuilds();
+  var single = [];
+  docs.forEach((doc) => {
+    if (doc.a === author) {
+      single.push(doc);
+    }
+  });
+  return single;
 }
 
 async function getSearch(query) {
-  var docs = await getBuilds()
-  var single = []
-  docs.forEach(doc => { if (doc.t.toLowerCase().includes(query)) { single.push(doc) } })
-  return single
+  var docs = await getBuilds();
+  var single = [];
+  docs.forEach((doc) => {
+    if (doc.t.toLowerCase().includes(query)) {
+      single.push(doc);
+    }
+  });
+  return single;
 }
 
-export { featuredDocs,singleRandomDoc, getSearch, getAuthor, getDocs, singleDoc, getBuilds, getCats, filterBuilds, singlePost, randomize, singleRandom };
+export {
+  featuredDocs,
+  singleRandomDoc,
+  getSearch,
+  getAuthor,
+  getDocs,
+  singleDoc,
+  getBuilds,
+  getCats,
+  filterBuilds,
+  singlePost,
+  randomize,
+  singleRandom,
+};
--- scripts/generateDb.js
+++ scripts/generateDb.js
@@ -1,34 +1,60 @@
-const matter = require('gray-matter');
-const sizeOf = require('image-size');
-const glob = require('glob');
-const sortArray = require('sort-array');
-const fs = require('fs');
-var md = require('markdown-it')({html:true,xhtmlOut:true});
+const matter = require("gray-matter");
+const sizeOf = require("image-size");
+const glob = require("glob");
+const sortArray = require("sort-array");
+const fs = require("fs");
+var md = require("markdown-it")({ html: true, xhtmlOut: true });
 
-function cartesian(...n){var r=[],t=n.length-1;return function a(e,u){for(var c=0,h=n[u].length;c<h;c++){var i=e.slice(0);i.push(n[u][c]),u==t?r.push(i):a(i,u+1)}}([],0),r}
-const getBuilds = async()=>{try{return(await import("../json/u.json")).default}catch(t){return console.log(t),[]}};
-function searchpo(e,r){return e.filter(e=>r.every(r=>e.g.split(",").some(e=>e===r)))}
+function cartesian(...n) {
+  var r = [],
+    t = n.length - 1;
+  return (
+    (function a(e, u) {
+      for (var c = 0, h = n[u].length; c < h; c++) {
+        var i = e.slice(0);
+        i.push(n[u][c]), u == t ? r.push(i) : a(i, u + 1);
+      }
+    })([], 0),
+    r
+  );
+}
+const getBuilds = async () => {
+  try {
+    const module = await import("../json/u.json", { assert: { type: "json" } });
+    return module.default;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+function searchpo(e, r) {
+  return e.filter((e) => r.every((r) => e.g.split(",").some((e) => e === r)));
+}
 
-const containerOptions = ['bucket','brute','tote','barrel','bin'];
-const lightingOptions = ['cfl','ufo','ledbulb','ledcustom'];
-const airflowOptions = ['pcfan','linefan','inlinefan'];
+const containerOptions = ["bucket", "brute", "tote", "barrel", "bin"];
+const lightingOptions = ["cfl", "ufo", "ledbulb", "ledcustom"];
+const airflowOptions = ["pcfan", "linefan", "inlinefan"];
 
-async function filterBuilds(num,page,sort,tags) {
-  var posts = await getBuilds()
+async function filterBuilds(num, page, sort, tags) {
+  var posts = await getBuilds();
 
   //Sort
-  if(sort === "asc" || "desc") { sortArray(posts, { by: 'd', order: sort }) }
-  if(sort === "pop") { 
-    posts.forEach(post => {post.v = Number(post.v) });
-    sortArray(posts, { by: 'v', order: 'desc' }) 
-  }  
-  if(sort === "rand") { 
+  if (sort === "asc" || "desc") {
+    sortArray(posts, { by: "d", order: sort });
+  }
+  if (sort === "pop") {
+    posts.forEach((post) => {
+      post.v = Number(post.v);
+    });
+    sortArray(posts, { by: "v", order: "desc" });
+  }
+  if (sort === "rand") {
     posts = shuffle(posts);
-  }  
+  }
 
   //Filter
   if (tags) {
-    var arrTags = tags.split(","); 
+    var arrTags = tags.split(",");
     posts = searchpo(posts, arrTags);
   }
 
@@ -36,75 +62,97 @@ async function filterBuilds(num,page,sort,tags) {
   if (page > 0) {
     posts = posts.slice(num * (page - 1), num * page);
   }
-  
-  return posts 
+
+  return posts;
 }
 
 async function createDb(src) {
   //Get post names
-  var folders = glob.sync('*', { cwd: `static/${src}/` })
+  var folders = glob.sync("*", { cwd: `static/${src}/` });
   var posts = [];
   //Main post loop
-  folders.forEach(singleFolder => {
-    const documentes = glob.sync('*.md', {cwd: `static/${src}/${singleFolder}`})
+  folders.forEach((singleFolder) => {
+    const documentes = glob.sync("*.md", {
+      cwd: `static/${src}/${singleFolder}`,
+    });
     let post = [];
 
     //Parse Markdown
-    var contents = matter(fs.readFileSync(`static/${src}/${singleFolder}/${documentes}`, 'utf8').toString());
-    post = contents.data
-    if(src === 'docs') {
+    var contents = matter(
+      fs
+        .readFileSync(`static/${src}/${singleFolder}/${documentes}`, "utf8")
+        .toString()
+    );
+    post = contents.data;
+    if (src === "docs") {
       post.c = md.render(post.c);
     }
     posts.push(post);
   });
 
   //Get Image references
-  posts.forEach(post => {
-    const srcImages = glob.sync('*.jpg', { cwd: `static/${src}/${post.s}` }).filter(e => e !== 'cover.jpg');    
+  posts.forEach((post) => {
+    const srcImages = glob
+      .sync("*.jpg", { cwd: `static/${src}/${post.s}` })
+      .filter((e) => e !== "cover.jpg");
     var postImages = [];
- 
-    srcImages.forEach(singleImage => {
-      var dimensions = sizeOf(`static/${src}/${post.s}/${singleImage}`);
-      var redimensions = (dimensions.height / dimensions.width * 100).toFixed(1);
-      redimensions = redimensions.replace(".0","")
 
-      postImages.push({ 'h': singleImage, 's': redimensions})
+    srcImages.forEach((singleImage) => {
+      var dimensions = sizeOf(`static/${src}/${post.s}/${singleImage}`);
+      var redimensions = ((dimensions.height / dimensions.width) * 100).toFixed(
+        1
+      );
+      redimensions = redimensions.replace(".0", "");
+
+      postImages.push({ h: singleImage, s: redimensions });
     });
-    post.i = postImages 
-  }); 
+    post.i = postImages;
+  });
 
   fs.writeFileSync(`./json/${src}.json`, JSON.stringify(posts));
-  console.log(`♥ ${src}.json generated`)
+  console.log(`♥ ${src}.json generated`);
 }
 
 async function createGallery(src) {
-  var folders = glob.sync('*', { cwd: `static/${src}/` })
+  var folders = glob.sync("*", { cwd: `static/${src}/` });
   var posts = [];
 
-  postRoutes = []
-  folders.forEach(singleFolder => {
-    const documentes = glob.sync('*.md', {cwd: `static/${src}/${singleFolder}`})
+  postRoutes = [];
+  folders.forEach((singleFolder) => {
+    const documentes = glob.sync("*.md", {
+      cwd: `static/${src}/${singleFolder}`,
+    });
     let post = {};
-    var contents = matter(fs.readFileSync(`static/${src}/${singleFolder}/${documentes}`, 'utf8').toString());
+    var contents = matter(
+      fs
+        .readFileSync(`static/${src}/${singleFolder}/${documentes}`, "utf8")
+        .toString()
+    );
     post.t = contents.data.t;
     post.s = contents.data.s;
-    post.g = contents.data.g; 
+    post.g = contents.data.g;
     post.v = contents.data.v;
-    post.z = contents.data.z.split(',').length;
+    post.z = contents.data.z.split(",").length;
     posts.push(post);
   });
 
   fs.writeFileSync(`./json/${src}-gallery.json`, JSON.stringify(posts));
-  console.log(`♥ ${src}-gallery.json generated`)
+  console.log(`♥ ${src}-gallery.json generated`);
 }
 
 async function createRoutes(src) {
-  var folders = glob.sync('*', { cwd: `static/${src}/` })
+  var folders = glob.sync("*", { cwd: `static/${src}/` });
   var postRoutes = [];
 
-  folders.forEach(singleFolder => {
-    const documentes = glob.sync('*.md', {cwd: `static/${src}/${singleFolder}`})
-    var contents = matter(fs.readFileSync(`static/${src}/${singleFolder}/${documentes}`, 'utf8').toString());
+  folders.forEach((singleFolder) => {
+    const documentes = glob.sync("*.md", {
+      cwd: `static/${src}/${singleFolder}`,
+    });
+    var contents = matter(
+      fs
+        .readFileSync(`static/${src}/${singleFolder}/${documentes}`, "utf8")
+        .toString()
+    );
     var post = `/${src}/${contents.data.s}`;
 
     postRoutes.push(post);
@@ -112,46 +160,64 @@ async function createRoutes(src) {
 
   fs.writeFileSync(`./json/${src}-routes.json`, JSON.stringify(postRoutes));
 
-  console.log(`♥ ${src}-routes.json generated`)
+  console.log(`♥ ${src}-routes.json generated`);
 }
 
 async function createCats() {
+  var containerCombi = cartesian(containerOptions);
+  var lightingCombi = cartesian(lightingOptions);
+  var airflowCombi = cartesian(airflowOptions);
 
-  var containerCombi = cartesian(containerOptions)
-  var lightingCombi = cartesian(lightingOptions)
-  var airflowCombi = cartesian(airflowOptions)
+  var containerLighting = cartesian(containerOptions, lightingOptions);
+  var containerAirflow = cartesian(containerOptions, airflowOptions);
+  var lightingAirflow = cartesian(lightingOptions, airflowOptions);
 
-  var containerLighting = cartesian(containerOptions,lightingOptions)
-  var containerAirflow = cartesian(containerOptions,airflowOptions)
-  var lightingAirflow = cartesian(lightingOptions,airflowOptions)
+  var megaCombinations = cartesian(
+    containerOptions,
+    lightingOptions,
+    airflowOptions
+  );
+  var allCombinations = [];
+  allCombinations = allCombinations.concat(
+    containerCombi,
+    lightingCombi,
+    airflowCombi,
+    containerLighting,
+    containerAirflow,
+    lightingAirflow,
+    megaCombinations
+  );
 
-  var megaCombinations = cartesian(containerOptions,lightingOptions,airflowOptions)
-  var allCombinations = []
-  allCombinations = allCombinations.concat(containerCombi,lightingCombi,airflowCombi,containerLighting,containerAirflow,lightingAirflow,megaCombinations)
-
-  var processCombinations = []
-  processCombinations.push('/gallery');
+  var processCombinations = [];
+  processCombinations.push("/gallery");
 
   for (let i = 0; i < allCombinations.length; i++) {
-    var megaposts = await filterBuilds(-1, 0, "pop",allCombinations[i].join(','));
-    var megapostLength = Math.ceil(megaposts.length / 36)
+    var megaposts = await filterBuilds(
+      -1,
+      0,
+      "pop",
+      allCombinations[i].join(",")
+    );
+    var megapostLength = Math.ceil(megaposts.length / 36);
 
     for (let e = 0; e < megapostLength; e++) {
-      processCombinations.push('/gallery/' + parseInt(e+1) +'/' + allCombinations[i].join('-'));
+      processCombinations.push(
+        "/gallery/" + parseInt(e + 1) + "/" + allCombinations[i].join("-")
+      );
     }
   }
 
   var alreposts = await filterBuilds(-1, 0, "pop");
-  var alrepostsLength = Math.ceil(alreposts.length / 36)
+  var alrepostsLength = Math.ceil(alreposts.length / 36);
   for (let f = 0; f < alrepostsLength; f++) {
-    processCombinations.push('/gallery/'+ parseInt(f+1));
-
+    processCombinations.push("/gallery/" + parseInt(f + 1));
   }
 
-
-  fs.writeFileSync(`./json/categories.json`, JSON.stringify(processCombinations));
-  console.log(`♥ categories.json generated`)
-
+  fs.writeFileSync(
+    `./json/categories.json`,
+    JSON.stringify(processCombinations)
+  );
+  console.log(`♥ categories.json generated`);
 }
 
 createDb("docs");
